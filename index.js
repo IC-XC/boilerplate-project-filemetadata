600fcff5aa3ef4568de8994c06542054121c2a2d
Per <vbratou@gmail.com>
2023-06-30 11:26:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const multer = require("multer");
+const bodyParser = require("body-parser");
+
+const upload = multer();
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const name = req.file.originalname;
+  const size = req.file.size;
+  const type = req.file.mimetype;
 
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
